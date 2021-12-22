d357cd9d81545c3a29a85b9353f03075844cc3bb
Lea Verou <lea@verou.me>
2021-12-22 14:06:36 +0200

    Add scoring function

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ document.addEventListener("click", evt => {
 });
 
 const allClasses = ["great", "good", "ok", "bad", "terrible"];
+const classified = "." + allClasses.join(", .")
 
 function classify(value, ...classes) {
 	value = Mavo.value(value);
+	let isNumeric = !isNaN(value);
 
 	if (value == "Unknown" || !value || !classes || classes.length === 0) {
 		return "";
@@ -25,35 +27,167 @@ function classify(value, ...classes) {
 
 	classes = Object.assign(...classes);
 
+	let breakpoints = allClasses.map(cl => classes[cl]);
+	let increasing;
+
+	if (isNumeric) {
+		// Are the breakpoints also numeric?
+		isNumeric = isNumeric && breakpoints.every(e => !isNaN(e));
+
+		// If increasing = true, this means the lower the value, the better the score
+		// I.e. great < good < ok < bad < terrible
+		increasing = breakpoints[0] < breakpoints[1];
+
+		if (isNumeric) {
+			if (increasing) {
+				breakpoints.push(Infinity);
+			}
+			else {
+				breakpoints.unshift(Infinity);
+			}
+
+			value = Number(value);
+		}
+	}
+
+	let ret;
+
+	let tiers = {};
+
+	for (let i=0; i < allClasses.length; i++) {
+		let cl = allClasses[i];
+
+		let breakpoint = Mavo.value(classes[cl]);
+
+		if (isNumeric) {
+			tiers[cl] = [
+				Number(breakpoints[i]),
+				Number(breakpoints[i + 1])
+			]
+
+		}
+		else {
+			tiers[cl] = breakpoint;
+		}
+	}
+
 	for (let cl of allClasses) {
 		let breakpoint = Mavo.value(classes[cl]);
 
-		if (breakpoint === null) {
+		if (breakpoint === null || breakpoint === "") {
 			delete classes[cl];
 		}
 		else if (breakpoint == value) {
 			return cl;
 		}
-	}
-
-	// If we're here, these are ranges
-	let breakpoints = Object.values(classes);
-	let increasing = breakpoints[0] < breakpoints[1];
-
-	for (let cl of allClasses) {
-		let breakpoint = classes[cl];
-
-		if (breakpoint == "*"){
-			// We need to do this on a second pass, because otherwise if it appears in the middle, we'd never get to anything after it
-			return cl;
+		else if (breakpoint == "*") {
+			ret = cl; // candidate return value, but we don't return yet because it may match other values exactly
 		}
+		else if (isNumeric) { // is numeric?
+			let [start, end] = tiers[cl];
 
-		if (!isNaN(value)) { // is numeric?
-			if (increasing && value < breakpoint || !increasing && value > breakpoint) {
-				return cl;
+			if (increasing) {
+				if (start < value && value < end) {
+					return cl;
+				}
+			}
+			else {
+				if (start > value && value > end) {
+					return cl;
+				}
 			}
 		}
-
-
 	}
+
+	if (ret !== undefined) {
+		return ret;
+	}
+
+	// If we're here, we're in a range, but not in any of the classes, extrapolate
+	if (isNumeric) {
+		let lastBreakpoint = breakpoints[breakpoints.length - 1];
+
+		if (increasing) {
+			if (value < breakpoints[0]) {
+				return "great"
+			}
+			else if (value > lastBreakpoint) {
+				return "terrible"
+			}
+		}
+		else {
+			if (value < breakpoints[0]) {
+				return "terrible"
+			}
+			else if (value > lastBreakpoint) {
+				return "great"
+			}
+		}
+	}
+}
+
+function classifyCommute(commute) {
+	commute = Mavo.value(commute);
+
+	if (!commute) {
+		return "";
+	}
+
+	let g = commute.match(/(?<mins>\d+) min (?<type>\w+)/)?.groups;
+
+	if (g.type === "walk") {
+		return g.mins < 9 ? "great" : "ok";
+	}
+	else if (g.type === "drive") {
+		return g.mins < 10 ? "bad" : "terrible"
+	}
+}
+
+function calculateScore(home, breakpoints) {
+	let element = home[Mavo.toNode].element;
+	let score = 0;
+	let analysis = [];
+
+	let weights = {address: 1, commute: 1};
+
+	for (let breakpoint of breakpoints) {
+		let {feature, weight} = breakpoint;
+
+		let prop = element.querySelector(`[title="${feature}"], [property="${feature.toLowerCase()}"]`);
+		let propName = prop.getAttribute("property") || prop.firstElementChild.getAttribute("property");
+
+
+		weights[propName] = weight;
+	}
+
+	for (let propName in weights) {
+		let weight = weights[propName];
+		let property = element.querySelector(`[property="${propName}"]`);
+
+		let wrapper = property.closest(".basic-info > div");
+
+		if (!wrapper) {
+			console.log(`Skipping ${propName}`)
+			continue;
+		}
+
+		let classification = wrapper.className;
+		let index = allClasses.indexOf(classification);
+
+		if (index === -1) {
+			continue;
+		}
+
+		let factor = -index + 2; // Convert 0 to 4 --> +2 to -2
+		// console.log(feature + "", factor, classification, +weight);
+
+		if (factor !== 0 && weight !== 0) {
+			analysis.push(`${factor} × ${weight} (${propName})`);
+		}
+
+
+		score += factor * weight;
+	}
+
+	return {score, analysis: analysis.join(" + ")};
 }
